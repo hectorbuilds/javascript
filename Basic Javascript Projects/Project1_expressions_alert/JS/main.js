823aa2a10c4a1de9a1be276ec54d2047cd589fa8
hectorbuilds <115192143+hectorbuilds@users.noreply.github.com>
2022-12-04 13:15:45 -0600

    push

---
+++ Basic Javascript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,16 @@
+alert()
+window.alert()
+var A ="This is a string"; //this variable is is being defined
+document.write(A) //this is a method that will plug in the variable inside the parenthesis 
+var H ="Hector Escobedo" //this variable is given its characteristics between quotations 
+window.alert(H)
+var X="I asked my brother,\"Show me how to box!\"";
+document.write(X)
+document.write("\"The do\'s and dont\'s are always on a list,"
++ "just be aware of what is on that list.\""
++ "- Anonymous");
+var B="concatenated"+"string"
+document.write(B)
+var G="Fender Guitar", D="Tama Drums", M="Shure 55";
+document.write(G)
+3+3
