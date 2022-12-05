e97dde9f8c11d88a5d3bf090e7371f39e1a0a2ac
hectorbuilds <115192143+hectorbuilds@users.noreply.github.com>
2022-12-04 21:25:10 -0600

    push

---
+++ Basic Javascript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    var sentence = "I am learning";
+    sentence +="a lot from this book!";
+    document.getElementByld("Concatenate").innerHTML = sentence;
+}
+var likes = 0;
+likes = 100;
+console.log(likes);
+likes+= 10;
