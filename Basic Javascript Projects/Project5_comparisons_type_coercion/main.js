26c24c222848abd98d9421d41e284fcd9d855468
hectorbuilds <115192143+hectorbuilds@users.noreply.github.com>
2022-12-13 10:48:16 -0600

    push

--- Basic Javascript Projects/Project5_comparisons_type_coercion/main.js
+++ Basic Javascript Projects/Project5_comparisons_type_coercion/main.js
@@ -2,3 +2,28 @@ document.write("60"+12)
 function my_Function(){
     document.getElementById("Test").innerHTML = isNaN ("This is a string!");
 }
+function my_Function2(){
+    var x= 29E320
+    document.getElementById("Test2").innerHTML = x;
+}
+function my_Function3(){
+    var y= -29E320
+    document.getElementById("Test3").innerHTML = y;
+}
+document.write(20>19)
+document.write(20<19)
+console.log (3+3+3)
+document.write(9==9)
+document.write(9==19)
+X=30
+Y=30
+document.write(X===Y)
+A="Thirty";
+B= 30
+document.write("Thirty"===30)
+document.write(9>3 && 3<9)
+document.write(9<90 || 30>90)
+document.write(90<9 || 9<3)
+function not_Function(){
+    document.getElementById("Not").innerHTML=!(30>9)
+}
