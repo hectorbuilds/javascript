fbd486d1323727a066eab99344e544b4a571c41d
hectorbuilds <115192143+hectorbuilds@users.noreply.github.com>
2022-12-23 18:09:52 -0600

    push

--- Basic Javascript Projects/Project7_scope_time_function/JS/main.js
+++ Basic Javascript Projects/Project7_scope_time_function/JS/main.js
@@ -40,3 +40,19 @@ function get_Date() {
         document.getElementById("guitars").innerHTML = "How are you today?";
     }
 }
+
+// time function
+
+function Time_function() {
+    var Time = new Date(). getHours();
+    var Reply;
+    if (Time <12 == Time > 0) { 
+        Reply = "It is morning time!";
+    }
+    else if (Time >= 12 == Time <18) {
+    }
+    else {
+        Reply = "It is evening time.";
+    }
+    document.getElementById("Time_of_day").innerHTML = Reply;
+}
