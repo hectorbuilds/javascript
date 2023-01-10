8dd5febede9200b470f44e6dc20d98ca75f3d45c
hectorbuilds <115192143+hectorbuilds@users.noreply.github.com>
2023-01-09 21:18:46 -0600

    push

---
+++ Basic Javascript Projects/Project10_loops_arrays/main.js
@@ -0,0 +1,66 @@
+function count_To_Ten() {
+    var Digit = "";
+    var X = 1;
+    while (X <10) {
+        Digit += "<br>" +X;
+    }
+    document.getElementById("Counting_to_Ten").innerHTML = Digit;
+}
+
+function dog_pics() {
+    var Dog_Picture = [];
+    Dog_Picture [0] = "running";
+    Dog_Picture [1] = "fetching";
+    Dog_Picture [2] = "walking";
+    Dog_Picture [3] = "sleeping";
+    document.getElementById("Dog").innerHTML = "In this picture, the dog is " +
+        Dog_Picture [2] + ".";
+}
+
+function constant_function() {
+    const Musical_Instrument = {type:"guitar", brand:"Fender", color:"black"};
+    Musical_Instrument.color = "blue";
+    Musical_Instrument.price = "$900";
+    document.getElementById("Constant").innerHTML = "The cost of the " +
+        Musical_Instrument.type + " was " + Musical_Instrument.price;
+}
+
+var X = 90;
+document.write(X);
+{
+    let X = 33;
+    document.write ("<br>" + X);
+}
+document.write("<br>" + X);
+
+function myFunction(name) {
+    return "Hello " + name;
+  }
+
+let car = {
+    make: "Dodge ",
+    model: "Viper ",
+    year: "2021 ",
+    color: "red ",
+    description : function () {
+        return "The car is a " + this.year + this.color + this.make + this.model;
+    }
+};
+document.getElementById("Car_Object").innerHTML = car.description();
+
+// brea/continue statements
+
+for (let i = 0; i < 10; i++) {
+    if (i === 3) { continue; }
+    text += "The number is " + i + "<br>";
+  }
+
+  const cars = ["BMW", "Volvo", "Saab", "Ford"];
+  list: {
+    text += cars[0] + "<br>";
+    text += cars[1] + "<br>";
+    break list;
+    text += cars[2] + "<br>";
+    text += cars[3] + "<br>";
+  }
+  
